80ef5e5fd0bad5d0c8a93f450a9c9df37a06c64b
Carson Davies <carsondavies@Carsons-MacBook-Pro.local>
2020-08-26 17:10:13 -0600

    media queries done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import './reset.css'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+
+  render() {
+    return (
+      <div className='homepage' img={'https://startbootstrap.github.io/startbootstrap-grayscale/assets/img/bg-masthead.jpg'}>
+        <Navbar />
+        <div className='title-text'>GRAYSCALE</div>
+        <div className='description'>A free, responsive, one page Bootstrap theme created by Start Bootsrap</div>
+        <button className='get-started'>GET STARTED</button>
+      </div>
+    )
+  }
 }
-
-export default App;
